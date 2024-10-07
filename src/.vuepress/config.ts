2ee713c23cc23e6fdda1f79161aecf151c51bdbf
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-10-08 09:38:03 +1030

    initialize blog

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,25 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/paggie",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Paggie's Blog",
+      description: "A template for Paggie's blog",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
